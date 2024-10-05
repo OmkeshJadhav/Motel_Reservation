3a48952618c0c2594f2849990e4e7190a7eed830
Omkesh Jadhav <iamomkeshjadhav@gmail.com>
2024-10-05 15:57:51 +0530

    Done Header, Date and Number of People

--- README.md
+++ README.md
@@ -1,7 +1,49 @@
-#Motel Booking
+# Motel Booking
 
-##Navbar
+## Home
+* Add components to be displayed
+
+
+## Navbar
 1. navContainer div
 2. Logo span
 3. div for navItems
 4. buttons inside navItems div
+
+## Header
+### Travel Content in Header
+1. header div
+2. headerContainer div
+3. headerList div
+4. headerListItems div for each item
+5. Fontawesome icon and then span for name 
+6. To show active item - Add additional classname and give css to it
+
+### Description in Header
+7. headerTitle div
+8. headerDescription div
+9. headerButton
+
+### Search Data and Person selection
+10. Install react-date-range library - Copy and paste its code and CSS
+    * <DateRange
+           editableDateInputs={true}
+           onChange={item => setDate([item.selection])}
+           moveRangeOnFirstSelection={false}
+           ranges={date}
+           className="date"
+      />
+    
+    * const [date, setDate] = useState([
+        {
+        startDate: new Date(),
+        endDate: new Date,
+        key: "selection",
+        },
+    ]);
+        
+11. Install date-fns library - for formatting dates
+    * Inside span : {`${format(date[0].startDate, "dd/mm/yyyy")} to ${format(date[0].endDate, "dd/mm/yyyy")}`} - formats date state of useState
+    * Below span, paste <DateRange/> component imported from react-date-range. - Then position it using CSS.
+
+12. To toggle visibility of Date calender - create useState and make changes accordingly
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,139 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./header.css";
+import {
+  faBed,
+  faCar,
+  faPerson,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { faCalendarDays } from "@fortawesome/free-regular-svg-icons";
+import { DateRange } from "react-date-range";
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // th
+import { useState } from "react";
+import { format } from "date-fns";
+
+const Header = () => {
+    const [dateVisible, setDateVisible] = useState(false)
+    const [date, setDate] = useState([
+        {
+        startDate: new Date(),
+        endDate: new Date,
+        key: "selection",
+        },
+    ]);
+
+    const [showNoOfPerson, setShowNoOfPerson] = useState(false)
+    const [chooseNoOfPerson, setChooseNoOfPerson] = useState({
+        adult: 1,
+        children: 0,
+        room: 1
+    })
+
+    const handleOption = (name, operation) => {
+        setChooseNoOfPerson(prev => {return {...prev, [name]: operation === "i" ? chooseNoOfPerson[name]+1 : chooseNoOfPerson[name]- 1}})
+    }
+
+
+    return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItems active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItems">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItems">
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car Rentals</span>
+          </div>
+          <div className="headerListItems">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport Taxis</span>
+          </div>
+          <div className="headerListItems">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+        </div>
+        <div>
+          <h1 className="headerTitle">Find your next stay</h1>
+          <p className="headerDescription">
+            Search low prices on hotels, homes and much more...
+          </p>
+          <button className="headerButton">Sign In / Register</button>
+        </div>
+
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon
+              icon={faCalendarDays}
+              className="headerIcon"
+            />
+            <span className="headerSearchText" onClick={()=>setDateVisible(!dateVisible) }>
+              {`${format(date[0].startDate, "dd/MM/yyyy")} to ${format(date[0].endDate, "dd/MM/yyyy")}`}
+            </span>
+            {dateVisible && <DateRange
+                editableDateInputs={true}
+                onChange={item => setDate([item.selection])}
+                moveRangeOnFirstSelection={false}
+                ranges={date}
+                className="date"
+            />}
+            
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText" onClick={()=>setShowNoOfPerson(!showNoOfPerson)}>
+              {`${chooseNoOfPerson.adult} Adult • ${chooseNoOfPerson.children} Children • ${chooseNoOfPerson.room} Room`}
+            </span>
+            {showNoOfPerson && <div className="options">
+                <div className="optionItem">
+                    <span className="optionText">Adult</span>
+                    <div className="optionCount">
+                        <button className="optionCounterButton" onClick={()=>handleOption("adult", "d")} disabled={chooseNoOfPerson.adult < 2}>-</button>
+                        <span className="optionCounterNumber">{chooseNoOfPerson.adult}</span>
+                        <button className="optionCounterButton" onClick={()=>handleOption("adult", "i")}>+</button>
+                    </div>
+                </div>
+                <div className="optionItem">
+                    <span className="optionText">Children</span>
+                    <div className="optionCount">
+                        <button className="optionCounterButton" onClick={()=>handleOption("children", "d")} disabled={chooseNoOfPerson.children == 0}>-</button>
+                        <span className="optionCounterNumber">{chooseNoOfPerson.children}</span>
+                        <button className="optionCounterButton" onClick={()=>handleOption("children", "i")}>+</button>
+                    </div>
+                </div>
+                <div className="optionItem">
+                    <span className="optionText">Room</span>
+                    <div className="optionCount">
+                        <button className="optionCounterButton" onClick={()=>handleOption("room", "d")} disabled={chooseNoOfPerson.room < 2}>-</button>
+                        <span className="optionCounterNumber">{chooseNoOfPerson.room}</span>
+                        <button className="optionCounterButton" onClick={()=>handleOption("room", "i")}>+</button>
+                    </div>
+                </div>
+            </div>}
+          </div>
+          <div className="headerSearchItem">
+            <button className="headerButton">Search</button>
+          </div>
+        </div>
+      </div> 
+    </div>
+  );
+};
+
+export default Header;
