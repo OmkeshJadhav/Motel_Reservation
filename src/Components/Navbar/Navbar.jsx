d101e490aaa3d07990ab77ec1f13c18b815aead5
Omkesh Jadhav <iamomkeshjadhav@gmail.com>
2024-10-06 21:32:15 +0530

    Navbar logo improved and home navigation added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom"
 import "./navbar.css"
 
 const Navbar = () => {
+
+  const navigate = useNavigate()
+
   return (
     <div className="navbar">
       <div className="container">
-        <span className="logo">Motel Booking</span>
+        <span className="logo" onClick={() => navigate("/")}>Motel Booking</span>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
